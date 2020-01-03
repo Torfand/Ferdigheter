2b49dcd94af5efefc0ceaa2cf20a2f2947aa457c
Torfand <torfin@getacademy.no>
2020-01-03 08:21:08 +0100

    update

--- Ferdigheter/Enkel Prototyping/Js/addActivity.js
+++ Ferdigheter/Enkel Prototyping/Js/addActivity.js
@@ -1,3 +1,50 @@
 function addActivity() {
+    document.getElementById("mainContent").innerHTML = `
+    <table>
+         <tr>
+                <td>Date</td>
+                <td><input id="eventDate" type="date" /></td>
+         </tr>
+         <tr>
+                <td>Description</td>
+                <td><input id="eventDesc" type="text" /></td>
+         </tr>
+         
+    </table>
+    <button onclick = "newActivity()">Register changes</button>
     
+    `;
+}
+
+function newActivity() {
+    var date = document.getElementById("eventDate").value;
+    var description = document.getElementById("eventDesc").value;
+        document.getElementById("mainContent").innerHTML =  `
+               <table>
+                    <tr>
+                        <th>Date</th>
+                        <th>Desctiption</th>
+                        <th>Number of signed up</th>
+                    </tr>
+                    <tr>
+                        <td>1.12.20</td>
+                        <td>Fake desc</td>
+                        <td>5</td>
+                    </tr>
+                    <tr>
+                       <td>1.12.20</td>
+                       <td>Fdasdasd</td>
+                       <td>30</td>
+                   </tr>
+                   <tr>
+                       <td>${date}</td>
+                       <td>${description}</td>
+                       <td>30</td>
+                   </tr>
+   
+               </table>
+     
+     
+     `
+   
 }
--- Ferdigheter/Enkel Prototyping/Js/showALL.js
+++ Ferdigheter/Enkel Prototyping/Js/showALL.js
@@ -1,20 +1,20 @@
 function showAll() {
-     document.getElementById("hei").innerHTML =  `
+     document.getElementById("mainContent").innerHTML =  `
             <table>
                  <tr>
                      <th>Date</th>
                      <th>Desctiption</th>
-                     <th>Signed up</th>
+                     <th>Number of signed up</th>
                  </tr>
                  <tr>
                      <td>1.12.20</td>
                      <td>Fake desc</td>
-                     <td>100</td>
+                     <td>5</td>
                  </tr>
                  <tr>
                     <td>1.12.20</td>
                     <td>Fdasdasd</td>
-                    <td>300000</td>
+                    <td>30</td>
                 </tr>
 
             </table>
