83fbd8a99176ca6ee24f0ab4c9fbd79a24e486cc
Torfand <57662236+Torfand@users.noreply.github.com>
2020-01-20 12:11:30 +0100

    x

---
+++ Klasser og objektorientert JS/Tre på rad/GameView.js
@@ -0,0 +1,21 @@
+class GameView {
+constructor(game) {
+    this._game = game;
+    this._gameBoard = document.getElementById('game');
+    let cells = game.getCell();
+    this._cellViews = cells.map( cell => new CellView(cell));
+
+}
+show() {
+    let boardHTML = this._cellViews.reduce((html, cellView)
+    => html + cellView.getHTML(), '');
+    let winnerHTML ='';
+    if(this._game.isStopped()) {
+       winnerHTML = `<h3 id="info">${this._game.getWinner()} Vant </h3>
+       <a id="link" href="javascript:gameController.restart();> Klikk her for og starte spillet på nytt
+       </a><br/>`  
+    }
+    this._gameBoard.innerHTML = winnerHTML + boardHTML;
+}
+
+}
