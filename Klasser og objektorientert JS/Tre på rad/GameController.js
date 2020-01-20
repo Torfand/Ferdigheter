63d1ed9a31d49853407259f848ad683edb5bda0b
Torfand <57662236+Torfand@users.noreply.github.com>
2020-01-20 10:52:25 +0100

    Update GameController.js

---
+++ Klasser og objektorientert JS/Tre på rad/GameController.js
@@ -0,0 +1,13 @@
+class GameController {
+    constructor(game) {
+        this._game = game;
+
+    }
+    restart() {
+        location.reload();
+    }
+    setX(index) {
+        this._game.setX(index);
+        updateView();
+    } 
+}
